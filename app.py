def50363f315711bbb41ee5aa4b25c09aae8630a
Shawn Robin <smrobin623@gmail.com>
2018-02-13 11:35:32 -0500

    edited regex call

--- app.py
+++ app.py
@@ -23,18 +23,14 @@ def aquadvisor():
 def parsed():
 	stats = aquadvisor()
 	bold = re.findall(r'<b>(.*?)</b>', stats)
-	speech = "Your aquarium filtration capacity is " + bold[0] + ". "
-	speech+=bold[2]
-	speech+="."
-	speech2 = "Your aquarium filtration capacity is " + bold[0] + ". " + bold[2] + "."
+	speech = "Your aquarium filtration capacity is " + bold[0] + ". " + bold[2] + "."
 	return jsonify(
 		{
 			"text":stats,
 			"type":type(stats).__name__,
 			"bold":bold,
 			"speech":speech,
-			"level":bold[2],
-			"speech2":speech2
+			"level":bold[2]
 		}
 	)
 
