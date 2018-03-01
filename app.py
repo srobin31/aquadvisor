376b4bede62c3b086885f0ef6b3a17ae7b60c9ca
Shawn Robin <smrobin623@gmail.com>
2018-03-01 08:40:26 -0500

    github authorized on heroku

--- app.py
+++ app.py
@@ -1,27 +1,14 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify
 from pyaqadvisor import Tank, Stocking
 import re
 
-import json
-
 app = Flask(__name__, static_url_path = "")
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    req = request.get_json(silent=True, force=True)
-    print(req)
-    print("Request:")
-    print(json.dumps(req, indent=4))
+	return "Yo, it's working!"
 
-    res = parse(aquadvisor())
-	#
-    # res = json.dumps(res, indent=4)
-    print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-#@app.route('/aquadvisor', methods=['GET', 'POST'])
+@app.route('/test', methods=['GET', 'POST'])
 def aquadvisor():
     stocking = Stocking().add('cardinal tetra', 5)\
                          .add('panda cory', 6)\
@@ -32,38 +19,40 @@ def aquadvisor():
     stocking_stats = t.get_stocking_level()
     return stocking_stats
 
-#@app.route('/parsed', methods=['GET', 'POST'])
-def parse(api_stats):
-    bold = re.findall(r'<b>(.*?)</b>', api_stats)
-    speech = "Your aquarium filtration capacity is " + bold[0] + ". " + bold[2] + "."
-	# return jsonify(
-	# 	{
-	# 		"speech":speech,
-    #         "displayText": speech,
-    #         "data": {},
-    #         "contextOut": [],
-    #         "source": ""
-	# 		# "text":stats,
-	# 		# "type":type(stats).__name__,
-	# 		# "bold":bold,
-	# 		# "speech":speech,
-	# 		# "level":bold[2]
-	# 	}
-	# )
-    return speech
+@app.route('/parsed', methods=['GET', 'POST'])
+def parsed():
+	stats = aquadvisor()
+	bold = re.findall(r'<b>(.*?)</b>', stats)
+	speech = "Your aquarium filtration capacity is " + bold[0] + ". " + bold[2] + "."
+	return jsonify(
+		{
+			"speech":speech,
+            "displayText": speech,
+            "data": {},
+            "contextOut": [],
+            "source": ""
+			# "text":stats,
+			# "type":type(stats).__name__,
+			# "bold":bold,
+			# "speech":speech,
+			# "level":bold[2]
+		}
+	)
 
-# @app.route('/unparsed', methods=['GET', 'POST'])
-# def unparsed():
-#     stats = aquadvisor()
-#     return jsonify(
-#         {
-#             "speech":stats,
-#             "displayText": stats,
-#             "data": {},
-#             "contextOut": [],
-#             "source": ""
-#         }
-#     )
+@app.route('/json', methods=['GET', 'POST'])
+def json():
+    stats = aquadvisor()
+	# bold = re.findall(r'<b>(.*?)</b>', stats)
+	# speech = "Your aquarium filtration capacity is " + bold[0] + ". " + bold[2] + "."
+    return jsonify(
+        {
+            "speech":stats,
+            "displayText": stats,
+            "data": {},
+            "contextOut": [],
+            "source": ""
+        }
+    )
 
 if __name__ == "__main__":
 	port = 9001
