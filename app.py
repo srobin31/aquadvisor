6243eafbf13af645620506d89e0c7e84d32f3d9c
Shawn Robin <smrobin623@gmail.com>
2018-03-05 11:00:22 -0500

    reformatted

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__, static_url_path = "")
 
 @app.route('/', methods=['GET'])
 def index():
-	return "Yo, it's working!"
+    return "Yo, it's working!"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-	req = request.get_json(silent=True, force=True)
+    req = request.get_json(silent=True, force=True)
 
     res = processRequest(req)
 
@@ -20,57 +20,33 @@ def webhook():
     return res
 
 def processRequest(req):
-	stocking = Stocking().add('cardinal tetra', 5)\
-                         .add('panda cory', 6)\
-                         .add('lemon_tetra', 12)\
-                         .add('pearl gourami', 4)
-
+    stocking = Stocking().add('cardinal tetra', 5)
     filter = req.get("result").get("parameters").get("filter")
-	t = Tank('55g').add_filter(filter).add_stocking(stocking)
-	aquadvisor = t.get_stocking_level()
-	return parse(aquadvisor)
+    t = Tank('55g').add_filter(filter).add_stocking(stocking)
+    aquadvisor = t.get_stocking_level()
+    return parse(aquadvisor)
 
 @app.route('/test', methods=['GET', 'POST'])
 def aquadvisor():
     stocking = Stocking().add('cardinal tetra', 5)\
-                         .add('panda cory', 6)\
-                         .add('lemon_tetra', 12)\
-                         .add('pearl gourami', 4)
+                        .add('panda cory', 6)\
+                        .add('lemon_tetra', 12)\
+                        .add('pearl gourami', 4)
 
     t = Tank('55g').add_filter("AquaClear 30").add_stocking(stocking)
     stocking_stats = t.get_stocking_level()
     return stocking_stats
 
 def parse(req):
-	bold = re.findall(r'<b>(.*?)</b>', stats)
-	speech = "Your aquarium filtration capacity is " + bold[0] + ". " + bold[2] + "."
-	return jsonify(
-		{
-			"speech":speech,
-            "displayText": speech,
-            "data": {},
-            "contextOut": [],
-            "source": ""
-			# "text":stats,
-			# "type":type(stats).__name__,
-			# "bold":bold,
-			# "speech":speech,
-			# "level":bold[2]
-		}
-	)
-
-def json(stats):
-	# bold = re.findall(r'<b>(.*?)</b>', stats)
-	# speech = "Your aquarium filtration capacity is " + bold[0] + ". " + bold[2] + "."
-    return jsonify(
-        {
-            "speech":stats,
-            "displayText": stats,
-            "data": {},
-            "contextOut": [],
-            "source": ""
-        }
-    )
+    bold = re.findall(r'<b>(.*?)</b>', stats)
+    speech = "Your aquarium filtration capacity is " + bold[0] + ". " + bold[2] + "."
+    return {
+        "speech":speech,
+        "displayText": speech,
+        "data": req,
+        "contextOut": [],
+        "source": "rocky-lowlands-15066"
+    }
 
 if __name__ == "__main__":
 	port = 9001
