97bfd33f9e2f480dab851ac919f58337234b86e9
Shawn Robin <smrobin623@gmail.com>
2018-03-15 08:48:45 -0400

    added tank size parameter to test webhook

--- app.py
+++ app.py
@@ -27,8 +27,10 @@ def testWebhook(req):
                         .add('panda cory', 6)\
                         .add('lemon_tetra', 12)\
                         .add('pearl gourami', 4)
+
+    tankSize = req.get("result").get("parameters").get("gallons")
     tankFilter = req.get("result").get("parameters").get("filter")
-    t = Tank('55g').add_filter(tankFilter).add_stocking(stocking)
+    t = Tank(tankSize).add_filter(tankFilter).add_stocking(stocking)
     stocking_stats = t.get_stocking_level()
     return parse(stocking_stats)
 
