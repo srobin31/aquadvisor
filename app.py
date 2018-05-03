e904ef8f36a9053462d48d8d664e5458930f2f48
Shawn Robin <smrobin623@gmail.com>
2018-05-03 08:51:01 -0400

    formatting response

--- app.py
+++ app.py
@@ -35,7 +35,8 @@ def parse(stats):
     filtCap = bold[0][:-1]
     speech = "Your recommended temperature range is" + ranges[0]
     speech += "\nYour recommended pH range is" + ranges[1] + "\n\n"
-    speech += "Your aquarium filtration capacity is " + filtCap + "%. " + bold[2] + ". "
+    speech += bold[2] + "."
+    speech += "Your aquarium filtration capacity is " + filtCap + "%. "
     if int(filtCap) < 90:
         speech += "Because your filtration capacity is less than 90%, we recommend that you get a more powerful filter."
     elif int(filtCap) > 90 and int(filtCap) < 110:
