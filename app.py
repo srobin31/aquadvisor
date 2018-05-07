00f605a01452cf5722023734801218c04fa5c683
Shawn Robin <smrobin623@gmail.com>
2018-05-07 08:44:13 -0400

    refactoring

--- app.py
+++ app.py
@@ -30,25 +30,19 @@ def getSpecs(req):
     return parse(stocking_stats)
 
 def parse(info):
-    warnings = re.findall(r'<li>(.*?)</li>', info)
-    ranges = re.findall(r'range:(.*?)</font>', info)
+    information = info()
+    information.ranges = re.findall(r'range:(.*?)</font>', info)
     stats = re.search('Your aquarium filtration.*\\.', info)
-    bold = re.findall(r'<b>(.*?)</b>', stats.group(0))
-    filtCap = bold[0][:-1]
-    speech = "Your recommended temperature range is" + ranges[0]
-    speech += "\nYour recommended pH range is" + ranges[1] + "\n\n"
-    speech += bold[1] + "."
-    speech += "\n\nYour aquarium filtration capacity is " + filtCap + "%. "
-    if int(filtCap) < 90:
-        speech += "Because your filtration capacity is less than 90%, we recommend that you get a more powerful filter."
-    elif int(filtCap) > 90 and int(filtCap) < 110:
-        speech += "Because your filtration capacity is around 100%, you have an okay filter. If you add more fish, we recommend upgrading to a stronger filter."
-    else:
-        speech += "Because your filtration capacity is above 110%, you're in good shape. However, you'll want to check again if you add more fish."
+    information.bold = re.findall(r'<b>(.*?)</b>', stats.group(0))
 
-    for warning in warnings:
+    filtCap = information.bold[0][:-1]
+
+    information.warnings = re.findall(r'<li>(.*?)</li>', info)
+    for warning in information.warnings:
         warning = re.sub(r'<.*?>', '', warning)
-        speech+=warning
+
+    speech = "You have " + information.warnings.length " warnings."
+
     return {
         "speech": speech,
         "displayText": speech,
@@ -56,6 +50,77 @@ def parse(info):
         "contextOut": [],
         "source": "rocky-lowlands-15066"
     }
+
+
+
+
+    # ranges = re.findall(r'range:(.*?)</font>', info)
+    # stats = re.search('Your aquarium filtration.*\\.', info)
+    # bold = re.findall(r'<b>(.*?)</b>', stats.group(0))
+    # filtCap = bold[0][:-1]
+    # speech = "Your recommended temperature range is" + ranges[0]
+    # speech += "\nYour recommended pH range is" + ranges[1] + "\n\n"
+    # speech += bold[1] + "."
+    # speech += "\n\nYour aquarium filtration capacity is " + filtCap + "%. "
+    # if int(filtCap) < 90:
+    #     speech += "Because your filtration capacity is less than 90%, we recommend that you get a more powerful filter."
+    # elif int(filtCap) > 90 and int(filtCap) < 110:
+    #     speech += "Because your filtration capacity is around 100%, you have an okay filter. If you add more fish, we recommend upgrading to a stronger filter."
+    # else:
+    #     speech += "Because your filtration capacity is above 110%, you're in good shape. However, you'll want to check again if you add more fish."
+    #
+    #
+    # return {
+    #     "speech": speech,
+    #     "displayText": speech,
+    #     "data": info,
+    #     "contextOut": [],
+    #     "source": "rocky-lowlands-15066"
+    # }
+
+def filtCapHelp(filtCap):
+    if filtCap < 90:
+        speech += "Because your filtration capacity is less than 90%, we recommend that you get a more powerful filter."
+    elif filtCap > 90 and filtCap < 110:
+        speech += "Because your filtration capacity is around 100%, you have an okay filter. If you add more fish, we recommend upgrading to a stronger filter."
+    else:
+        speech += "Because your filtration capacity is above 110%, you're in good shape. However, you'll want to check again if you add more fish."
+
+class info(object):
+    def __init__(self):
+        self._ranges = None
+        self._bold = None
+        self._warnings = None
+
+    @property
+    def ranges(self):
+        """I'm the 'x' property."""
+        return self._ranges
+
+    @property
+    def bold(self):
+        """I'm the 'x' property."""
+        return self._bold
+
+    @property
+    def warnings(self):
+        """I'm the 'x' property."""
+        return self._warnings
+
+    @ranges.setter
+    def ranges(self, value):
+        #print("setter of x called")
+        self._ranges = value
+
+    @bold.setter
+    def ranges(self, value):
+        #print("setter of x called")
+        self._bold = value
+
+    @warnings.setter
+    def warnings(self, value):
+        #print("setter of x called")
+        self._warnings = value
 
 if __name__ == "__main__":
 	port = 9001
